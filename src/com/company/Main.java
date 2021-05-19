615955267cc478581d72a67678d46f8f7c9d9615
Laura Vikanis <vikanis@sipgate.de>
2021-05-19 14:03:57 +0200

    floet + double

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,42 +5,56 @@ import java.awt.*;
 public class Main {
 
     public static void main(String[] args) {
-    int myValue = 1000;
+        int myValue = 1000;
+        System.out.println(myValue);
 
-    int myMinValue = Integer.MIN_VALUE;
-    int myMaxValue = Integer.MAX_VALUE;
-    System.out.println("Integer Min Value =" + myMinValue);
-    System.out.println("Integer Max Value =" + myMaxValue);
-    System.out.println("Busted Min Value =" + (myMinValue -1 ));
-    System.out.println("Busted MAX value =" + (myMaxValue + 1 ));
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+        System.out.println("Integer Min Value =" + myMinValue);
+        System.out.println("Integer Max Value =" + myMaxValue);
+        System.out.println("Busted Min Value =" + (myMinValue - 1));
+        System.out.println("Busted MAX value =" + (myMaxValue + 1));
 
-    // next line error "int number is to long"
-    // int myMaxIntTest = 21474836474
-    // Int can also be written with underscores
-    // int myMaxIntTest = 21_474_836_44;
+        // next line error "int number is to long"
+        // int myMaxIntTest = 21474836474
+        // Int can also be written with underscores
+        // int myMaxIntTest = 21_474_836_44;
 
-    byte myMinByteValue = Byte.MIN_VALUE;
-    byte myMaxByteValue = Byte.MAX_VALUE;
-    System.out.println("Byte Min Value =" + myMinByteValue);
-    System.out.println("Byte Max Value =" + myMaxByteValue);
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Min Value =" + myMinByteValue);
+        System.out.println("Byte Max Value =" + myMaxByteValue);
 
-    short myMinShortValue = Short.MIN_VALUE;
-    short myMaxShortValue = Short.MAX_VALUE;
-    System.out.println("Short Min Value =" + myMinShortValue);
-    System.out.println("Short Max Value =" + myMaxShortValue);
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Min Value =" + myMinShortValue);
+        System.out.println("Short Max Value =" + myMaxShortValue);
 
-    int myTotal = (myMinValue / 2);
-    byte myNewBytevalue = (byte) (myMinByteValue / 2);
-    //casting other types o value
+        int myTotal = (myMinValue / 2);
+        byte myNewBytevalue = (byte) (myMinByteValue / 2);
+        //casting other types o value
 
-    short myNewShortValue = (short) (myMaxShortValue/2);
+        short myNewShortValue = (short) (myMaxShortValue / 2);
 
-    //--------
+        //--------
 
-byte newByteValue = 100;
-short newShortValue = 32;
-int newIntValue = 234;
-long newLongValue =  (50000L +(10L*(newByteValue + newIntValue + newShortValue)));
-    System.out.print(newLongValue);
+        byte newByteValue = 100;
+        short newShortValue = 32;
+        int newIntValue = 234;
+        long newLongValue = (50000L + (10L * (newByteValue + newIntValue + newShortValue)));
+        System.out.print(newLongValue);
+
+        //--------
+
+        float myFloatMinValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println(myMaxFloatValue + "+" + myFloatMinValue);
+
+        //--------
+
+        double myMinDoubleMinValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println(myMaxDoubleValue + "+" + myMinDoubleMinValue);
+
     }
 }
